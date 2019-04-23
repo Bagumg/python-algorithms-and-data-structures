86e6adf7d73721f53bf97e18917cf8e3a5197d6a
Yriy <41549026+Bagumg@users.noreply.github.com>
2019-04-23 23:08:04 +0700

    Delete test.py

--- lesson_1/test.py
+++
@@ -1,8 +0,0 @@
-digit_one = [int(x) for x in input('Введите первое число: ')]
-digit_two = [int(x) for x in input('Введите второе число: ')]
-if sum(digit_one) > sum(digit_two):
-    print(''.join(map(str, digit_one)))
-    print(sum(digit_one))
-else:
-    print(''.join(map(str, digit_two)))
-    print(sum(digit_two))
